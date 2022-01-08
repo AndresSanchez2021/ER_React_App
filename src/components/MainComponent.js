d0cbc23bfdbd0f12019c1f0ca86e1ccb1023de8d
AndresSanchez2021 <anfsanchezcu@unal.edu.co>
2022-01-08 00:12:36 -0500

    header n footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './MenuComponent';
 import DishdetailComponent  from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
 
 
@@ -20,13 +22,10 @@ class Main extends Component{
   render(){
     return (
       <div className="App">
-        <Navbar dark color="primary">
-            <div className="container">
-            <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
-            </div>
-        </Navbar>
+        <Header/>
         <Menu dishes={this.state.dishes} onClick ={(dishId)=>this.onDishSelect(dishId)} />
         <DishdetailComponent dish={this.state.dishes.filter((e)=>e.id===this.state.selectedDish)}/>
+        <Footer/>
       </div>
     );
   }
